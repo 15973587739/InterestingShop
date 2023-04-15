224588816a23b93ca54e40a594595809e1788b63
Siyu <2823005269@qq.com>
2023-04-15 14:37:59 +0800

    趣买买Mybatis改造完成

--- src/main/java/cn/interestingshop/web/manager/NoticeServlet.java
+++ src/main/java/cn/interestingshop/web/manager/NoticeServlet.java
@@ -19,6 +19,8 @@ public class NoticeServlet extends AbstractServlet {
 
 	private NoticeService noticeService;
 
+
+	@Override
 	public void init() throws ServletException {
 		this.noticeService = new NoticeServiceImpl();
 	}
